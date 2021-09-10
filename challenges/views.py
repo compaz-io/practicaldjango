eae6afb71ff4823771d8dcd4d0522f8baff5ce6d
Schweigs <mjschweiger@gmail.com>
2021-09-10 12:15:54 +0100

    use dictionary as data store for http responses

--- challenges/views.py
+++ challenges/views.py
@@ -3,18 +3,28 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+monthly_challenges = {
+    'january': '<body>Eat no meat for the entire month!</body>',
+    'february': '<body>Walk for at least 20 minutes every day!</body>',
+    'march': '<body>Learn Django for at least 20 minutes every day!</body>',
+    'april': '<body>Learn Django for at least 20 minutes every day!</body>',
+    'may': '<body>Learn Django for at least 20 minutes every day - may!</body>',
+    'june': '<body>Learn Django for at least 20 minutes every day! - june</body>',
+    'july': '<body>Learn Django for at least 20 minutes every day! - july</body>',
+    'august': '<body>Learn Django for at least 20 minutes every day! - august</body>',
+    'september': '<body>Learn Django for at least 20 minutes every day! - sep</body>',
+    'october': '<body>Learn Django for at least 20 minutes every day! oct</body>',
+    'november': '<body>Learn Django for at least 20 minutes every day! nov</body>',
+    'december': '<body>Learn Django for at least 20 minutes every day! dec</body>',
+}
+
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
-def monthly_challenges(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = "<body>Eat no meat for the entire month!</body>"
-    elif month == 'february':
-        challenge_text ="<body>Walk for at least 20 minutes every day!</body>"
-    elif month == 'march':
-        challenge_text = "<body>Learn Django for at least 20 minutes every day!</body>"
-    else:
-        return HttpResponseNotFound("<body>This month is not supported!</body>")
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("Month is not supported")
 
     return HttpResponse(challenge_text)
