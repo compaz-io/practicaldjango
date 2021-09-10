f0ce6eee6852d171673f3a0ef1ee43ec5a59949b
Schweigs <mjschweiger@gmail.com>
2021-09-10 11:56:21 +0100

    refactor with dynamic view function

--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("<body>Eat no meat for the entire month!</body>")
-
-def february(request):
-    return HttpResponse("<body>Walk for at least 20 minutes every day!</body>")
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "<body>Eat no meat for the entire month!</body>"
+    elif month == 'february':
+        challenge_text ="<body>Walk for at least 20 minutes every day!</body>"
+    elif month == 'march':
+        challenge_text = "<body>Learn Django for at least 20 minutes every day!</body>"
+    else:
+        return HttpResponseNotFound("<body>This month is not supported!</body>")
+        
+    return HttpResponse(challenge_text)
